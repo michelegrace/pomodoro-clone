3f7fb937ed1010c01619efa5ae5ac5b437624571
Michele <michelesmolensky@practicecafe.com>
2019-04-19 14:47:14 -0500

    Finished the Pomodoro Timer
    - Refactored the code
    
    - Added in a long break button each 4th run of the work timer and fixed a bug where the long break button was immediately available at the beginning of the 4th cycle instead of at the end.
    
    - Added a hopefully not-annoying chime at the end of each alarm.

--- app.js
+++ app.js
@@ -1,65 +1,94 @@
 let runWorkTimer = 0;
 let runBreakTimer = 0;
-let workDuration = 1 * 60;
-let breakDuration = 1 * 60;
+let workDuration = 25 * 60;
+let breakDuration = 5 * 60;
+let longBreakDuration = 20 * 60;
+let timesWorkTimerRan = 0;
 let minutes;
 let seconds;
 
 const display = document.querySelector('#clock');
 const startWork = document.querySelector('#timer-btn');
 const startBreak = document.querySelector('#break-btn');
+const startLongBreak = document.querySelector('#long-break-btn');
+const alarmBell = new Audio('glass_ping.mp3');
 
-const body = document.querySelector('body');
+let controlTimer = (duration)=>{
+    minutes = parseInt(duration/60);
+    seconds = parseInt(duration % 60);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+}
 
-let pomodoroWorkTimer = function(){
-  clearInterval(runBreakTimer);
-  minutes = parseInt(workDuration / 60);
-  seconds = parseInt(workDuration % 60);
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
-
-  body.className="test";
-
-  workDuration -= 1;
-  display.innerHTML = minutes + ":" + seconds;
+let workTimer = function(){
+    controlTimer(workDuration);
+    workDuration -= 1;
+    display.innerHTML = minutes + ":" + seconds;
 
   if (workDuration < 0) {
     clearInterval(runWorkTimer);
     if (workDuration == -1) {
-      workDuration = 25 * 60;
-      startBreak.disabled = false;
+      alarmBell.play();
+        workDuration = 25 * 60;
+        startBreak.disabled = false;
+        startLongBreak.disabled = false;
     }
   }
 }
 
-let pomodoroBreakTimer = function(){
-  clearInterval(runWorkTimer);
-  minutes = parseInt(breakDuration/60);
-  seconds = parseInt(breakDuration % 60);
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
+let shortBreakTimer = function(){
+    controlTimer(breakDuration);
+    breakDuration -= 1;
+    display.innerHTML = minutes + ":" + seconds;
 
-  breakDuration -= 1;
-  display.innerHTML = minutes + ":" + seconds;
-
-  if(breakDuration < 0){
+  if (breakDuration < 0){
     clearInterval(runBreakTimer);
-    if(breakDuration == -1){
-      //probably a hack-y reset
-      breakDuration = 1 * 60;
+    if (breakDuration == -1){
+      alarmBell.play();
+        breakDuration = 5 * 60;
+        startWork.disabled = false;
+    }
+  }
+}
+
+let longBreakTimer = function(){
+    controlTimer(longBreakDuration);
+
+    longBreakDuration -= 1;
+    display.innerHTML = minutes + ":" + seconds;
+
+  if(longBreakTimer < 0) {
+    clearInterval(runLongBreakTimer);
+    if(longBreakDuration == -1){
+      longBreakDuration = 20 * 60;
       startWork.disabled = false;
     }
   }
 }
 
 startWork.addEventListener('click', function(){
-  clearInterval(runWorkTimer);
+    clearInterval(runWorkTimer);
+    timesWorkTimerRan ++;
+      if (timesWorkTimerRan % 4 == 0) {
+        startLongBreak.classList.remove('btn-hidden');
+        startBreak.classList.add('btn-hidden');  
+      } else {
+        startBreak.classList.remove('btn-hidden');
+        startLongBreak.classList.add('btn-hidden');
+      }
   startBreak.disabled = true;
-  runWorkTimer = setInterval(pomodoroWorkTimer, 1000, display);
+  startLongBreak.disabled = true;
+  runWorkTimer = setInterval(workTimer, 1000, display);
 });
 
 startBreak.addEventListener('click', function(){
-  clearInterval(pomodoroBreakTimer);
+  clearInterval(runBreakTimer);
   startWork.disabled = true;
-  runBreakTimer = setInterval(pomodoroBreakTimer, 1000, display);
+  runBreakTimer = setInterval(shortBreakTimer, 1000, display);
 });
+
+startLongBreak.addEventListener('click', function(){
+  clearInterval(runLongBreakTimer);
+  startWork.disabled = true;
+  runLongBreakTimer = setInterval(longBreakTimer, 1000, display);
+})
